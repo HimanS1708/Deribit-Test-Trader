cb743f3e7d02521fc37539964742eec9c272ae32
HimanS1708 <myanshim17@gmail.com>
2024-12-02 14:54:26 +0530

    Project template setup done

---
+++ setup.ps1
@@ -0,0 +1,30 @@
+if (!(Test-Path "vcpkg/.git")) {
+    git submodule update --init --recursive vcpkg
+}
+
+.\vcpkg\bootstrap-vcpkg.bat
+
+$env:VCPKG_ROOT="$pwd/vcpkg"
+$env:PATH="$env:VCPKG_ROOT;$env:PATH"
+
+if (!(vcpkg list | Select-String "nlohmann-json")) {
+    vcpkg install nlohmann-json:x64-windows
+}
+
+if (!(vcpkg list | Select-String "websocketpp")) {
+    vcpkg install nlohmann-json:x64-windows
+}
+
+if (Test-Path "build") {
+    Remove-Item -Path "build" -Recurse -Force
+}
+
+mkdir build
+cd build
+
+cmake .. -G "MinGW Makefiles"
+mingw32-make
+
+./DeribitTestTrader
+
+cd ..
