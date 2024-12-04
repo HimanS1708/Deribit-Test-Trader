73d7da68d2975c853fb383e9fcf3770a5b7abf60
HimanS1708 <myanshim17@gmail.com>
2024-12-04 20:07:58 +0530

    Fixed build script

--- setup_and_run.ps1
+++ setup_and_run.ps1
@@ -7,21 +7,10 @@ if (!(Test-Path "vcpkg/.git")) {
 $env:VCPKG_ROOT="$pwd/vcpkg"
 $env:PATH="$env:VCPKG_ROOT;$env:PATH"
 
-if (!(vcpkg list | Select-String "openssl")) {
-    vcpkg install openssl:x64-mingw-static
-}
-
-if (!(vcpkg list | Select-String "boost-thread")) {
-    vcpkg install boost-thread:x64-windows
-}
-
-if (!(vcpkg list | Select-String "nlohmann-json")) {
-    vcpkg install nlohmann-json:x64-windows
-}
-
-if (!(vcpkg list | Select-String "websocketpp")) {
-    vcpkg install nlohmann-json:x64-windows
-}
+vcpkg install openssl:x64-windows
+vcpkg install boost-thread:x64-windows
+vcpkg install nlohmann-json:x64-windows
+vcpkg install websocketpp:x64-windows
 
 if (Test-Path "build") {
     Remove-Item -Path "build" -Recurse -Force
